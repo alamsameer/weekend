64cf6c8caa7a80777e64a1f2eda7d4fbc2f1695c
Sameer Alam <alamsameer738@gmail.com>
2024-08-25 16:00:29 +0530

    added code for the 100 days of code

---
+++ 100codedays/waitinglist/index.js
@@ -0,0 +1,5 @@
+document.getElementById('waitlistForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thanks for joining the waitlist! We\'ll notify you when we launch.');
+    // Add form submission logic here (e.g., send data to the server)
+});
